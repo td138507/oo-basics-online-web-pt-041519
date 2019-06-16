0a1f98d3e19d616b38153178cd5a769e8a8e0a07
Taylor Doyle <tcd56@cornell.edu>
2019-06-16 03:56:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
 # Make your shoe class here!
+class shoe
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def brand
+    @brand
+  end
+
+  def shoe= (shoe)
+    @shoe = shoe
+  end
+
+  def shoe
+    @shoe
+  end
+  
