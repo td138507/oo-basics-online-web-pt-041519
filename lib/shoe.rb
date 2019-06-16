abf28db2c7080d25713a2e6f554ad1c90101a440
Taylor Doyle <tcd56@cornell.edu>
2019-06-16 04:00:07 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -12,6 +12,14 @@ def color
   @color
 end
 
+def color=(color)
+  @color = color
+end
+
+def size
+  @size
+end
+
 def size= (num)
   @size = num
 end
@@ -20,7 +28,14 @@ def material
   @material
 end
 
+def material=(material)
+  @material = material
+end
+
 def condition
   @condition
 end
+
+def condition=(condition)
+  @condition = condition
 end
